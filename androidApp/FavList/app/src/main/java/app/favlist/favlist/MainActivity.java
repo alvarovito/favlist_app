d760098696a8cba4c714eb36704575126c856517
Álvaro Sáez <alsamon1@hotmail.com>
2018-12-05 00:40:35 +0100

    styles

--- androidApp/FavList/app/src/main/java/app/favlist/favlist/MainActivity.java
+++ androidApp/FavList/app/src/main/java/app/favlist/favlist/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements OnListItemInterac
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
